c762632486ca6319c4236ec4f20957fffa8553b7
Arthur Panchenko <vrthurrain@gmail.com>
2024-08-22 16:38:54 +0300

    annotate & aggregate

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
 
-from .models import Subscription
+from .models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = (
+            '__all__'
+        )
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    plan = PlanSerializer()
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.price
 
     class Meta:
         model = Subscription
@@ -16,5 +29,6 @@ class SubscriptionSerializer(serializers.ModelSerializer):
             'email',
             'plan',
             'plan_id',
-            'service'
+            'service',
+            'price'
         )
--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
@@ -15,6 +15,22 @@ class SubscriptionView(ReadOnlyModelViewSet):
                 'user__username',
             )
         )
+    ).select_related('plan').annotate(
+        price=F('service__full_price') - 
+              F('service__full_price') * 
+              F('plan__discount_percent') / 100
     )
     
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        qs = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {'result': response.data}
+        response_data['total_amount'] = qs.aggregate(
+            total=Sum('price')
+        ).get('total')
+        response.data = response_data
+
+        return response
